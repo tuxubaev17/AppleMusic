feb55cca1795bd88272c6a94a75902039ba6b54f
Anastasiia Maksimova <sia.m94@ya.ru>
2021-11-10 20:58:58 -0500

    Initial Commit

---
+++ AppleMusic_SwiftUI/AppleMusic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_SwiftUIApp.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Anastasiia on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
