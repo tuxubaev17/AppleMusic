ada702a6303a2c3e43297f6534f90bd5ae40a694
Anastasiia Maksimova <sia.m94@ya.ru>
2021-11-20 14:45:15 -0500

    Create StationItemView.

---
+++ AppleMusic_SwiftUI/Modules/Radio/View/StationItemView.swift
@@ -0,0 +1,49 @@
+//
+//  StationItemView.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Anastasiia on 11/20/21.
+//
+
+import SwiftUI
+
+struct StationItemView: View {
+    
+    private let stationItems = StationItemsModels.data
+    private let columns = [GridItem(.flexible())]
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            LazyVGrid(columns: columns, alignment: .leading) {
+
+                ForEach(stationItems, id: \.self) { data in
+                    HStack() {
+
+                        Image(data.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(5)
+                            .frame(width: 100)
+
+                        VStack(alignment:.leading) {
+
+                            Text(data.name)
+                                .font(.title3)
+
+                            Text(data.title)
+                                .font(.system(size: 14))
+                                .foregroundColor(.gray)
+                        }.padding()
+                    }
+                    Divider()
+                }
+            }.padding([.leading, .trailing])
+        }
+    }
+}
+
+struct StationItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        StationItemView()
+    }
+}
