4e33f1c5b3d87475dfe6bd1300506e7991598bf8
Anastasiia Maksimova <sia.m94@ya.ru>
2021-11-20 14:04:04 -0500

    Create and configure StationItemsModels.

---
+++ AppleMusic_SwiftUI/Modules/Radio/Model/StationsItemsModel.swift
@@ -0,0 +1,37 @@
+//
+//  StationsItemsModel.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Anastasiia on 11/20/21.
+//
+
+import Foundation
+
+struct StationItemsModels {
+    var image: String
+    var name: String
+    var title: String
+}
+
+extension StationItemsModels: Hashable {
+    static var data = [
+        StationItemsModels(image: "blonded",
+                           name: "blonded RADIO",
+                           title: "Демонстрация электичного вкуса"),
+        StationItemsModels(image: "stmpd",
+                           name: "STMPD Radio",
+                           title: "Эксклюзивные треки"),
+        StationItemsModels(image: "fatboyslim",
+                           name: "Fatboy Slim Radio",
+                           title: "Классика 90-х и эксклюзивные бутлеги"),
+        StationItemsModels(image: "the echo chamber",
+                           name: "The Echo Chamber",
+                           title: "Слушаем музыку вместе с Mike D"),
+        StationItemsModels(image: "one mix",
+                           name: "One Mix",
+                           title: "Микс от лучшых в электронной музыке"),
+        StationItemsModels(image: "st.vincents",
+                           name: "St.Vincent's",
+                           title: "Сингер-сонграйтер рекомендует")
+    ]
+}
